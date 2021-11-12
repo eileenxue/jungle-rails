b8dbcdda505574d2f87838e69eaa8a31f2f969b1
Eileen Xue <xue.eileen@gmail.com>
2021-11-12 19:53:24 +0000

    Add users controller and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#show'
 
+  # Add signup/users routes
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
